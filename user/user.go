e196234c45eb6f578d8301c9d8010d7283e83147
DoudiNCer <doudi@outlook.lv>
2023-03-01 13:40:40 +0800

    feat: 添加获取指定用户对象的方法

--- user/user.go
+++ user/user.go
@@ -1,6 +1,8 @@
 package user
 
-// 用于构建可以@的用户
+import "github.com/DodiNCer/lrcat-go/message"
+
+// User 用于构建可以@的用户
 type User struct {
 	Tag string `json:"tag"`
 	// 用户ID，使用Open ID
@@ -9,18 +11,25 @@ type User struct {
 	UserName string
 }
 
-// 获取可插入到文本消息的代码
+// TextMsgUserJson 获取可插入到文本消息的代码
 func (user User) TextMsgUserJson() string {
 	return "<at user_id=\"" + user.UserId + "\">" + user.UserName + "</at>"
 }
 
-// 获取可插入到富文本消息的结构体
+// RichTextMsgUserObject 获取可插入到富文本消息的结构体
 func (user User) RichTextMsgUserObject() interface{} {
-	user.Tag = "at"
+	user.Tag = message.TAG_AT
 	return user
 }
 
+// GetAtAllUser 获取用于@全体的 User 对象
 func GetAtAllUser() (user User) {
 	var u = User{UserId: "all", UserName: "所有人"}
+	return u
+}
+
+// GetAtUser 获取@某个用户的用户对象
+func GetAtUser(userId, userName string) (user User) {
+	var u = User{UserId: userId, UserName: userName}
 	return u
 }
