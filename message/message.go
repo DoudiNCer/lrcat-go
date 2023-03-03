950190806b6ee1c15fad2a3a4dc0864f50e26188
DoudiNCer <doudi@outlook.lv>
2023-03-03 22:05:36 +0800

    feat: 添加全局开关 & 修改注释格式

--- message/message.go
+++ message/message.go
@@ -8,10 +8,10 @@ const (
 	MESSACE_CARD_MESSAGE_TYPE = "interactive"
 )
 
-// 消息体
+// Message 消息体
 type Message interface {
-	// 获取用于发送的消息体
+	// GetMsgJson 获取用于发送的消息体
 	GetMsgJson() (string, error)
-	// 签名消息
+	// SignMsg 签名消息
 	SignMsg(key string)
 }
--- message/text.go
+++ message/text.go
@@ -6,15 +6,15 @@ import (
 	"time"
 )
 
-// 普通文本消息
+// Text 普通文本消息
 type Text struct {
 	Timestamp string                 `json:"timestamp,omitempty"`
 	Sign      string                 `json:"sign,omitempty"`
-	Msg_type  string                 `json:"msg_type"`
+	MsgType   string                 `json:"msg_type"`
 	Content   map[string]interface{} `json:"content"`
 }
 
-// 添加消息内容
+// AddContent 添加消息内容
 func (text *Text) AddContent(msg string) {
 	text.Content = map[string]interface{}{
 		TEXT_MESSAGE_TYPE: msg,
@@ -22,8 +22,8 @@ func (text *Text) AddContent(msg string) {
 }
 
 func (text Text) GetMsgJson() (string, error) {
-	if text.Msg_type == "" {
-		text.Msg_type = TEXT_MESSAGE_TYPE
+	if text.MsgType == "" {
+		text.MsgType = TEXT_MESSAGE_TYPE
 	}
 	textJson, err := json.Marshal(text)
 	if err != nil {
